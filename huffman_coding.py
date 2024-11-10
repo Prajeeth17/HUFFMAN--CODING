3cb67f499317c5054c457c222f8603f783beae8b
Prajeeth <120513885+Prajeeth17@users.noreply.github.com>
2024-11-11 01:59:49 +0530

    Update huffman_coding.py

--- huffman_coding.py
+++ huffman_coding.py
@@ -1,18 +1,46 @@
-
-
-# Get the input String
-string =
-
-# Create tree nodes
+string = 'Prajeeth'
 class NodeTree(object):
+    
+    def __init__(self, left=None, right=None):
+        self.left = left 
+        self.right  = right
 
-# Main function to implement huffman coding
-def huffman_code_tree(node, left=True, binString=''):
+    def children(self):
+        return (self.left,self.right)
 
-# Calculate frequency of occurrence
+def huffman_code_tree(node, left=True, binString=''): 
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    D= dict()
+    D.update(huffman_code_tree(l, True, binString + '0'))
+    D.update(huffman_code_tree(r, False, binString + '1'))
+    return D
+
 freq = {}
 
-# Print the characters and its huffmancode
-huffmanCode = huffman_code_tree(nodes[0][0])
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
 
-print(' Char | Huffman code ')
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+nodes = freq
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+    
+    nodes = sorted(nodes, key=lambda x: x[1],reverse=True)
+
+
+huffmanCode = huffman_code_tree (nodes[0][0])
+print('Char | Huffman code ')
+print('---------------------')
+
+for (char, frequency) in freq:
+    print('%-4r %12s' % (char, huffmanCode[char]))
